7a51b6c16602a93629d70bd96cb59d6094cef9d9
unknown <juanchav@hotmail.com>
2023-04-18 20:20:30 -0500

    kmedoids

--- unsupervised/k_medoids.py
+++ unsupervised/k_medoids.py
@@ -1,74 +1,135 @@
 import numpy as np
-import matplotlib.pyplot as plt
-class KMedoidsClass:
-    def __init__(self,data,k,iters):
-        self.data= data
-        self.k = k
-        self.iters = iters
-        self.medoids = np.array([data[i] for i in range(self.k)])
-        self.colors = np.array(np.random.randint(0, 255, size =(self.k, 4)))/255
-        self.colors[:,3]=1
 
-    def manhattan(self,p1, p2):
-        return np.abs((p1[0]-p2[0])) + np.abs((p1[1]-p2[1]))
+class K_Medoids:
 
-    def get_costs(self, medoids, data):
-        tmp_clusters = {i:[] for i in range(len(medoids))}
-        cst = 0
-        for d in data:
-            dst = np.array([self.manhattan(d, md) for md in medoids])
-            c = dst.argmin()
-            tmp_clusters[c].append(d)
-            cst+=dst.min()
+    def __init__(self,X=None,K=None,max_iters=1000):
+        self.X=X
+        self.K=K
+        self.max_iters=max_iters
+        self.distances=None
+        self.X_in_clusters=None
+        np.random.seed(123)
+    
+    def fit(self,X,K,max_iters):
+        self.X=X
+        self.K=K
+        self.max_iters=max_iters
+        # Inicializar los medoides aleatoriamente
+        n = self.X.shape[0]
+        self.medoids_idx = np.random.choice(n, size=self.K, replace=False)
+        self.medoids = self.X[self.medoids_idx, :]
 
-        tmp_clusters = {k:np.array(v) for k,v in tmp_clusters.items()}
-        return tmp_clusters, cst
+        # Inicializar la asignación de clústeres
+        self.labels = np.zeros(n)
 
-    def fit(self):
+        for i in range(self.max_iters):
+            # Asignar cada punto al clúster con el medoide más cercano
+            # Tomando cada valor y calculando la distancia con el punto del cluster mas cercano
+            # Generando una matriz de x,d,k, sobre la que se suman las ditancias de las componentes 
+            # y se genera con estas sumas una nueva matriz de X,K que indica la menor distancia 
+            self.distances = np.sum((self.X[:, :, np.newaxis] - self.medoids.T[np.newaxis, :, :]) ** 2, axis=1)
+            new_labels = np.argmin(self.distances, axis=1)
 
-        samples,_ = self.data.shape
-
-        self.clusters, cost = self.get_costs(data=self.data, medoids=self.medoids)
-        count = 0
-
-        colors =  np.array(np.random.randint(0, 255, size =(self.k, 4)))/255
-        colors[:,3]=1
-
-        plt.title(f"Step : 0")
-        [plt.scatter(self.clusters[t][:, 0], self.clusters[t][:, 1], marker="*", s=100,
-                                        color = colors[t]) for t in range(self.k)]
-        plt.scatter(self.medoids[:, 0], self.medoids[:, 1], s=200, color=colors)
-        plt.show()
-
-        while True:
-            swap = False
-            for i in range(samples):
-                if not i in self.medoids:
-                    for j in range(self.k):
-                        tmp_meds = self.medoids.copy()
-                        tmp_meds[j] = i
-                        clusters_, cost_ = self.get_costs(data=self.data, medoids=tmp_meds)
-
-                        if cost_<cost:
-                            self.medoids = tmp_meds
-                            cost = cost_
-                            swap = True
-                            self.clusters = clusters_
-                            print(f"Medoids Changed to: {self.medoids}.")
-                            plt.title(f"Step : {count+1}")  
-                            [plt.scatter(self.clusters[t][:, 0], self.clusters[t][:, 1], marker="*", s=100,
-                                        color = colors[t]) for t in range(self.k)]
-                            plt.scatter(self.medoids[:, 0], self.medoids[:, 1], s=200, color=colors)
-                            plt.show()
-            count+=1
-
-            if count>=self.iters:
-                print("End of the iterations.")
-                break
-            if not swap:
-                print("No changes.")
+            # Verificar si se alcanzó la convergencia, si no cambia quiere decir que el modelo converge
+            #y se hallo la respuesta optima
+            if np.array_equal(self.labels, new_labels):
                 break
 
-dt = np.random.randint(0,100, (100,2))
-kmedoid = KMedoidsClass(dt,5,5)
-kmedoid.fit()
+            # Actualizar la asignación de clústeres
+            self.labels = new_labels
+            
+            # self.medoids_idx = np.where(new_labels == [k for k in self.K])[0]
+
+            # self.medoids_idx = np.array([np.where(self.labels == j)[0][np.argmin(distances[:, j])] for j in range(self.K)])
+            # Actualizar los medoides
+            for j in range(self.K):
+                # Obtener los puntos en el clúster j
+                # indica los indices de los puntos que pertenecen al cluster
+                indices = np.where(self.labels == j)[0]
+                #trae los registros de X
+                cluster = self.X[indices, :]
+
+                # Calcular la distancia total de cada punto a los demás puntos del clúster
+                self.distances = np.sum((cluster[:, :, np.newaxis] - cluster.T[np.newaxis, :, :]) ** 2, axis=1)
+
+                # Seleccionar el punto con la distancia total más baja como el nuevo medoide
+                self.medoids_idx[j] = indices[np.argmin(np.sum(self.distances, axis=1))]
+                # new_medoid_idx = indices[np.argmin(np.sum(distances, axis=1))]
+                self.medoids[j, :] = self.X[self.medoids_idx[j], :]
+
+        # Keep the data clustered              
+        self.X_in_clusters= self.transform(self.X)
+
+    def transform(self,X):
+        
+        distances = np.sum((X[:, :, np.newaxis] - self.X[self.medoids_idx, :].T[np.newaxis, :, :]) ** 2, axis=1)
+        return np.argmin(distances, axis=1)
+         
+    
+    def fit_transform(self,X,K,max_iters):
+        self.fit(X,K,max_iters)
+        return self.X_in_clusters
+
+    def distance_whit_others_points(self,k=None,a=True):
+        ''' a = True uses to calculate the distances in the same cluster'''
+        if k==None:
+            k=self.K-1
+        non_cluster=None
+        indices = np.where(self.labels == k)[0]
+        cluster = self.X[indices, :] 
+        if a:
+            non_cluster = self.X[indices, :]                  
+        else:
+            indices = np.where(self.labels != k)[0]  
+            non_cluster = self.X[indices, :]          
+        #trae los registros de X
+
+        if k>0:
+            distance=np.sum(np.sum((cluster[:, :, np.newaxis] - non_cluster.T[np.newaxis, :, :]) ** 2, axis=1),axis=1)
+            return np.concatenate((distance,self.distance_whit_others_points(k=k-1,a=a)),axis=0)
+        else:
+            return np.sum(np.sum((cluster[:, :, np.newaxis] - non_cluster.T[np.newaxis, :, :]) ** 2, axis=1),axis=1)
+        
+        
+    def distance_whit_others_points2(self,k=None):
+        ''' a = True uses to calculate the distances in the same cluster'''
+        if k==None:
+            k=self.K-1        
+        distance_in_cluster=None
+        distance_non_cluster=None
+
+        #takes de rows that belongs to cluster K
+        indices = np.where(self.labels == k)[0]   
+        cluster = self.X[indices, :]
+
+        #takes de rows that do not belongs to cluster K
+        indices = np.where(self.labels != k)[0] 
+        non_cluster = self.X[indices, :]
+
+        if k>0:
+            #distance in cluster
+            distance_in_cluster=np.sum(np.sum((cluster[:, :, np.newaxis] - cluster.T[np.newaxis, :, :]) ** 2, axis=1),axis=1)
+
+            #distance without  cluster
+            distance_non_cluster=np.sum(np.sum((cluster[:, :, np.newaxis] - non_cluster.T[np.newaxis, :, :]) ** 2, axis=1),axis=1)
+            matrix = np.concatenate((distance_in_cluster.reshape(-1, 1), distance_non_cluster.reshape(-1, 1)), axis=1)       
+            return np.concatenate((matrix,self.distance_whit_others_points2(k-1)),axis=0)
+        else:
+            distance_in_cluster=np.sum(np.sum((cluster[:, :, np.newaxis] - cluster.T[np.newaxis, :, :]) ** 2, axis=1),axis=1)
+            distance_non_cluster=np.sum(np.sum((cluster[:, :, np.newaxis] - non_cluster.T[np.newaxis, :, :]) ** 2, axis=1),axis=1)
+            matrix = np.concatenate((distance_in_cluster.reshape(-1, 1), distance_non_cluster.reshape(-1, 1)), axis=1)   
+            return matrix
+
+    def silhouette_coefficent(self):
+        sc=self.distance_whit_others_points2()
+        coef = np.zeros((sc.shape[0]))
+        for i in range(sc.shape[0]):
+            if sc[i][0]<sc[i][1]:
+                coef[i]=1-(sc[i][0]/sc[i][1])
+            if sc[i][0]==sc[i][1]:
+                coef[i]=0
+            if sc[i][0]>sc[i][1]:
+                coef[i]=(sc[i][1]/sc[i][0])-1
+
+        return coef
+
